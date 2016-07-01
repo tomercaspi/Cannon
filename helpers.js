900ed52023968b3dad5850ac7ac0bf5ce599aeec
tomercaspi <tomercaspi@gmail.com>
2016-07-01 14:03:13 +0300

    Adding files to project

---
+++ helpers.js
@@ -0,0 +1,83 @@
+(function(global) {
+  'use strict';
+
+  /**
+   * this function help you create and control an animation
+   * @param element
+   * @param callback(element, index) - when return true stop the animation
+   * @param timeout between time frames
+   * @return global.helper object (for chaining)
+   */
+  function animate(el, callback, timeout) {
+    var time = timeout || 300;
+
+    setTimeout(function t(timeoutEl, i) {
+      var ret = callback(el, timeout);
+      if(!ret)
+        setTimeout(t, time, timeoutEl, i + 1);
+    }, 0, el, 0);
+
+    return global.helper;
+  }
+
+  /**
+   * this function change the css of an element
+   * @param el
+   * @param css - string
+   * @return global.helper object (for chaining)
+   */
+  function changeCss(el, css) {
+    console.log("this.el from the ChangeCSS function" + this.el);
+    var prop = dashToCamelcase(css.split(':')[0]);
+    var value = css.split(':')[1];
+
+    el.style[prop] = value;
+
+    return global.helper;
+  }
+
+  /**
+   * this function remove the dash and replace it with upper case
+   * @param string (with dashs)
+   * @return string (camelcase)
+   */
+  function dashToCamelcase(string) {
+    var words = string.split('-');
+
+    for (var i = 1; i < words.length; i++) {
+      words[i] = words[i][0].toUpperCase() + words[i].substr(1, words[i].length);
+    }
+
+    return words.join('');
+  }
+
+  /**
+   * this function check if 2 element collide
+   * @param element 1
+   * @param element 2
+   * @return boolean
+   */
+  function isCollide(el1, el2) {
+
+    var rect1 = el1.getBoundingClientRect();
+    var rect2 = el2.getBoundingClientRect();
+
+    return !!(rect1.top >= rect2.top &&
+            rect1.top <= rect2.top + rect2.height &&
+            rect1.left >= rect2.left &&
+            rect1.left <= rect2.left + rect2.width) ||
+            (rect1.top + rect1.height >= rect2.top &&
+            rect1.top + rect1.height <= rect2.top + rect2.height &&
+            rect1.left + rect1.width >= rect2.left &&
+            rect1.left + rect1.width <= rect2.left + rect2.width);
+  }
+
+/////////////////////////////////////////////////////////
+
+  global.helper = {
+    changeCss: changeCss,
+    animate: animate,
+    isCollide: isCollide
+  }
+
+}(window));
---
+++ script.js
@@ -0,0 +1,90 @@
+
+// getElementByClassName returns array of elements, not single element, as getElementById. So it is needed to point to the element you want
+// var barrel = document.getElementsByClassName('cannonBarrel')[0];
+var up = document.getElementsByClassName('buttonUp')[0];
+var down = document.getElementsByClassName('buttonDown')[0];
+var shoot = document.getElementsByClassName('buttonShoot')[0];
+var shooting = document.getElementById('shooting');
+// var deg = 0;
+
+/*up.addEventListener('click',moveCannonUp, false);
+down.addEventListener('click',moveCannonDown, false);
+shoot.addEventListener('click',fireCannon, false);*/
+
+var cannon = {
+    deg: 0,
+    barrel: document.getElementsByClassName('cannonBarrel')[0],
+    addDegree: function() {
+        this.deg++;
+        this.render();
+    },
+    decreaseDegree: function() {
+        this.deg--;
+        this.render();
+    },
+    render: function() {
+        this.barrel.setAttribute('style', 'transform: rotate(' + cannon.deg + 'deg);')
+    },
+    fireCannon: function() {
+        var ball = new Ball();
+        ball.fire(shooting);
+    }
+}
+
+up.addEventListener('click', function(){
+    cannon.addDegree();
+});
+
+down.addEventListener('click', function(){
+    cannon.decreaseDegree();
+});
+
+shoot.addEventListener('click', function(){
+    cannon.fireCannon();
+});
+
+/*
+function moveCannonUp() {
+    deg+=2;
+    barrel.setAttribute('style', 'transform:rotate(' + deg + 'deg);');
+}
+
+function moveCannonDown() {
+    deg-=2;
+    barrel.setAttribute('style', 'transform:rotate(' + deg + 'deg);');
+    }
+
+*/
+
+/*function fireCannon() {
+    var ball = new Ball();
+    ball.fire(shooting);
+
+
+    // helper.animate(ball, function(){
+    //
+    // },1000)
+}*/
+
+function Ball() {
+    this.el = document.createElement('div');
+    this.el.className = "ball";
+}
+
+Ball.prototype.fire = function(node) {
+    node.appendChild(this.el);
+    this.moveBall();
+}
+
+Ball.prototype.moveBall = function(){
+    var that = this.el;
+    var sum = 0;
+    helper.animate(that, function(el, i){
+        sum +=i * 10;
+        helper.changeCss(el,'transform: translate('+ sum + 'px, '+ sum+ 'px);');
+        return this.el;}
+        ,10)
+
+/*    horizontalDistance = that.power * Math.cos(Math.toRadians(that.deg)) * time;
+    verticalDistance = (that.power * time * Math.sin(Math.toRadians(that.deg))) - (0.5 * preference.g * Math.pow(time,2));*/
+}
